7c56c5592571088a4105aaccbff90f95893a4752
Variodriver <59835749+Variodriver@users.noreply.github.com>
2025-12-01 17:26:21 +0100

    Fixed rounding age

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-/*
-let names = [
-    {
-        gender: "male",
-        names: ["Jan", "Miroslav", "Petr", "Michal", "Milan", "Antonio", "Tobiáš", "Josef", "Tim", "Vítek", "Vít", "Richard", "Dalibor", "Libor", "Jevhen", "Daniel", "Dan", "Oleg", "Tomáš", "Miro", "Jiří", "Pavel", "Pavol", "Beno", "Roman", "Jakub", "Václav", "Makar", "Ivan", "Marek", "Martin", "Matěj", "Marcel", "Adam", "Mikuláš", "Zdeněk", "Přemysl", "Jonáš", "David", "Rudolf", "Kryštof", "Michael", "Ondřej", "Vladimír", "Antonín", "Vilém", "Luděk", "Lukáš", "Albert", "Vladislav"],
-        surnames: ["Moravec", "Samek", "Briscein", "Hliněnský", "Lehotský", "Horák", "Šokalo", "Hájek", "Tolaš", "Kořínek", "Bartoš", "Sulženko", "Hoza", "Bruckler", "Kubáň", "Markvart", "Blachut", "Matoušek", "Nekoranec", "Veber", "Král", "Nový", "Novotný", "Novák", "Zima", "Skočdopole", "Lenčéš", "Votrubec", "Černý", "Červený", "Zeman", "Zikmund", "Velebný", "Patera", "Čada", "Houška", "Hrachovec", "Cihlář", "Vaník", "Krystl", "Makovec", "Sloup", "Kalaš", "Panc", "Friesl", "Marek", "Boháč", "Bozděch", "Válek", "Staněk"]
-    },
-
-    {
-        gender: "female",
-        names: ["Ivana", "Radka", "Pavla", "Iveta", "Miroslava", "Petra", "Milena", "Jarmila", "Jaroslava", "Marcela", "Johana", "Jasmína", "Barbora", "Natálie", "Amélie", "Simona", "Michaela", "Sofie", "Jitka", "Leona", "Jana", "Lenka", "Kristýna", "Adriana", "Dana", "Karolína", "Irena", "Vanessa", "Jindřiška", "Alina", "Eliška", "Lucie", "Magdaléna", "Olga", "Anežka", "Dominika", "Aneta", "Hedvika", "Marie", "Alžběta", "Gabriela", "Antonie", "Milada", "Vilma", "Veronika"],
-        surnames: ["Vachovcová", "Veberová", "Nováková", "Novotná", "Nová", "Sehnoutková", "Hájková", "Urbanová", "Milá", "Černá", "Jíránková", "Poláčková", "Kopřivová", "Smetana", "Jalovcová", "Bařáková", "Limberská", "Vavrochová", "Šavelová", "Nerudová", "Zapletalová", "Zahradníková", "Palacká", "Votrubcová", "Lopatová", "Radová", "Gebertová", "Kroupová", "Kinjo", "Šrejmová", "Zavadilová", "Janečková", "Janáčková", "Zlámalová", "Drápelová", "Donutilová", "Čermáková", "Patejdlová", "Boháčová", "Pohsoltová"]
-    }
-];
- */
-
-// Reduced names (bcs there is limit in tests)
 let names = [
     {
         gender: "male",
@@ -153,7 +136,7 @@ export function getEmployeeStatistics(dtoIn) {
     }
 
     dtoOut.averageAge = agesSum / allAges.length;
-    dtoOut.averageAge = dtoOut.averageAge.toFixed(1);
+    dtoOut.averageAge = dtoOut.averageAge.toPrecision(2);
 
     allAges.sort();
     allWorkloads.sort();
@@ -194,3 +177,11 @@ export function main(dtoIn) {
     return dtoOut;
 }
 
+
+console.log(main({
+    count: 50,
+    age: {
+        min: 4,
+        max: 50
+    }
+}));
